39e4a9df0c72b388cbf82cecec40df04e93ff37d
alihussain.m <alihussain.m@tataelxsi.co.in>
2022-01-28 09:46:20 +0530

    Button Handling in League Activity - lesson-7

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -3,17 +3,46 @@ package com.example.swoosh
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague=""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
+    fun onMensClicked(view : View){
+        womensLeagueBtn.isChecked=false
+        coEdLeagueBtn.isChecked=false
+        selectedLeague=mensLeagueBtn.text.toString()
+    }
+
+    fun onWomensClicked(view : View){
+        mensLeagueBtn.isChecked=false
+        coEdLeagueBtn.isChecked=false
+        selectedLeague=womensLeagueBtn.text.toString()
+    }
+
+    fun onCoEdClicked(view : View){
+        mensLeagueBtn.isChecked=false
+        womensLeagueBtn.isChecked=false
+        selectedLeague=coEdLeagueBtn.text.toString()
+    }
+
     fun leagueNextClicked(view : View){
-        val skillActivity= Intent(this,SkillActivity::class.java)
-        startActivity(skillActivity)
+        if( selectedLeague !="") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE,selectedLeague)
+            startActivity(skillActivity)
+        }else{
+            Toast.makeText(this,"Please select a league.",Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
